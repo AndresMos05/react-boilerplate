29ca6b17b6c948fe9c88d4a8a3abfb7ccbf99444
AndresMos05 <andres.mosquera@kommit.co>
2020-04-29 16:39:39 -0500

    adding git hooks

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import '@testing-library/jest-dom'
 import App from './App'
 describe('App', () => {
-    it('Renders without error', () => {
-        render(<App />)
-
-    });
-
-
-});
+  it('Renders without error', () => {
+    render(<App />)
+  })
+})
