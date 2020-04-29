db60bc192beb0aff942a92fcc8c21a07500a617a
AndresMos05 <andres.mosquera@kommit.co>
2020-04-29 17:50:52 -0500

    finish

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ import ReactDOM from 'react-dom'
 import '@babel/polyfill'
 import App from './App'
 import './styles.css'
+import DefaulErrorBoundary from './DefaultErrorBoundary'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+if (process.env.NODE_ENV === 'development') {
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaulErrorBoundary>
+      <App />
+    </DefaulErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('app')
+)
