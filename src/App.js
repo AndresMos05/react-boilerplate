db434affc48e3a97a81695fff204d4fdc47ab933
AndresMos05 <andres.mosquera@kommit.co>
2020-04-29 15:37:26 -0500

    test working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {hot} from 'react-hot-loader'
+import { hot } from 'react-hot-loader'
 
+const Warning = React.lazy(() => import('./Warning'))
 class App extends React.Component {
     state = {
         count: 0
@@ -18,6 +19,11 @@ class App extends React.Component {
                 </h2>
                 <button onClick={this.increment}>+</button>
                 <button onClick={this.decrement}>-</button>
+                {count > 10 ?
+                    <React.Suspense fallback={null}>
+                        <Warning />
+                    </React.Suspense>
+                    : null}
             </div>
 
         )
