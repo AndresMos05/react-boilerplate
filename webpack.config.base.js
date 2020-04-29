0ec0d254206b639854cb10adaa1886add87c0172
AndresMos05 <andres.mosquera@kommit.co>
2020-04-29 10:09:23 -0500

    video 17

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const HtmlWebpackPlugin= require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -14,8 +14,13 @@ module.exports = {
                 exclude: /node_modules/,
                 options: {
                     presets: ['@babel/preset-env', '@babel/preset-react'],
-                    plugins: ['@babel/plugin-proposal-class-properties']
+                    plugins: ['react-hot-loader/babel', '@babel/plugin-proposal-class-properties']
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /node_modules/
             }
         ]
     },
